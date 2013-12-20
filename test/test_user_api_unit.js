467cab76b1f26da2c67f86c459ed7591c8a5950c
kentquirk <kentquirk@gmail.com>
2013-12-20 15:35:54 -0800

    Checkpoint; nothing actually works right but I need to share it to get some help.

--- test/test_user_api_unit.js
+++ test/test_user_api_unit.js
@@ -68,3 +68,116 @@ describe('GET /nonexistent', function() {
      });
 
 });
+
+describe('POST /status', function() {
+
+    it('should respond with 404', function(done) {
+        supertest(userapi.server)
+        .post('/status')
+        .expect(404);
+        done();
+     });
+
+});
+
+describe('GET /user/auth with an invalid ID/token', function() {
+
+    it('should respond with 401', function(done) {
+        supertest(userapi.server)
+        .get('/user/auth')
+        .set('X-Tidepool-Application-ID', 'badid')
+        .set('X-Tidepool-API-Token', 'abcdef1234567890')
+        .expect(401);
+        done();
+     });
+
+});
+
+describe('POST /user/auth with a valid ID/token', function() {
+
+    it('should respond with 200 and a session token', function(done) {
+        supertest(userapi.server)
+        .get('/user/auth')
+        .set('X-Tidepool-Application-ID', 'realid')
+        .set('X-Tidepool-API-Token', 'abcdef1234567890')
+        .expect(200)        
+        .end(function(err, res) {
+            expect(err).to.not.exist;
+            expect('Content-Type', 'application/json');
+            expect('X-Tidepool-Session-Token', /[a-f0-9]{16}/);
+            done();
+        });
+
+     });
+
+});
+
+describe('GET /login', function() {
+
+    it('should respond with 404', function(done) {
+        supertest(userapi.server)
+        .get('/login')
+        .expect(404);
+        done();
+     });
+
+});
+
+describe('POST /user without any data', function() {
+
+    it('should respond with 400', function(done) {
+        supertest(userapi.server)
+        .post('/status')
+        .expect(400);
+        done();
+     });
+
+});
+
+describe('GET /login', function() {
+
+    it('should respond with 404', function(done) {
+        supertest(userapi.server)
+        .get('/login')
+        .expect(404);
+        done();
+     });
+
+});
+
+describe('POST /user for a new user minimal fields and no conflict', function() {
+
+    it.skip('should respond with 200', function(done) {
+        supertest(userapi.server)
+        .post('/status')
+        .send({
+            username: 'newuser',
+            password: 'newuser'
+        })
+        .accept('application/json')
+        .expect(200)
+        .end(function(err, res) {
+            expect(err).to.not.exist;
+            expect(res.text).to.equal('"Ok"');
+            done();
+        });
+     });
+
+});
+
+
+describe('POST /login', function() {
+
+    it.skip('guest with no PW should respond with 200', function(done) {
+        supertest(userapi.server)
+        .post('/status')
+        .send({ username: 'guest', password: '' })
+        .expect(200)
+        .end(function(err, res) {
+            expect(err).to.not.exist;
+            expect(res.text).to.equal('"Ok"');
+            done();
+        });
+     });
+
+});
