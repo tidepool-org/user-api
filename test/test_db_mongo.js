425e293b9af34e28a984168814293f12ed0605b0
kentquirk <kentquirk@gmail.com>
2013-12-27 15:02:33 -0800

    clean up api and tests

--- test/test_db_mongo.js
+++ test/test_db_mongo.js
@@ -13,8 +13,8 @@ describe('db_mongo basics', function() {
     it('should have addUser method', function() {
         expect(dbmongo).to.respondTo('addUser');
     });
-    it('should have getUserFromIdPw method', function() {
-        expect(dbmongo).to.respondTo('getUserFromIdPw');
+    it('should have getUser method', function() {
+        expect(dbmongo).to.respondTo('getUser');
     });
     it('should have deleteUser method', function() {
         expect(dbmongo).to.respondTo('deleteUser');
@@ -24,42 +24,81 @@ describe('db_mongo basics', function() {
 describe('db_mongo', function() {
 
     before(function(done) {
-        // the special config value we pass will wipe the database
         dbmongo.init({
             mongo_connection_string: 'mongodb://localhost/test',
+            // the special config value we pass for testing will wipe the database
             _wipeTheEntireDatabase: true,
             adminkey: 'specialkey'
         }, done);
     });
 
+    var user1 = {userid: 'Testy', email: 'mctesty@mctester.com', pwhash: 'test2', extras: {}};
+    var user2 = {userid: 'McTesty', email: 'mctesty@tester.com', pwhash: 'test', extras: {junk: 'bar'}};
+
+    var checkResult = function(user, ref) {
+        expect(user).to.have.property('userid');
+        expect(user.userid).to.equal(ref.userid);
+        expect(user).to.not.have.property('pwhash');
+        expect(user).to.have.property('emails');
+        expect(user.emails.length).to.equal(1);
+        expect(user.emails[0]).to.equal(ref.email);
+        expect(user).to.have.property('extras');
+        expect(user.extras).to.eql(ref.extras);
+    };
+
+
     it('should create a user', function(done) {
-        dbmongo.addUser({userid: 'Testy', email: 'testy@mctester.com', pwhash: 'test', extras: {junk: 'foo'}}, function(result){
+        dbmongo.addUser(user1, function(result) {
             expect(result.success).to.be.true;
-            expect(result.user).to.have.property('userid');
-            expect(result.user.userid).to.equal('Testy');
-            expect(result.user).to.not.have.property('pwhash');
-            expect(result.user).to.have.property('emails');
-            expect(result.user.emails.length).to.equal(1);
-            expect(result.user.emails[0]).to.equal('testy@mctester.com');
-            expect(result.user).to.have.property('extras');
-            expect(result.user.extras.junk).to.equal('foo');
+            checkResult(result.user, user1);
             done();
         });
     });
 
     it('should fail trying to recreate existing user', function(done) {
-        dbmongo.addUser({userid: 'Testy', email: 'mctesty@mctester.com', pwhash: 'test2', extra: {}}, function(result){
+        dbmongo.addUser(user1, function(result){
             expect(result.success).to.be.false;
             done();
         });
     });
 
     it('should create a second user', function(done) {
-        dbmongo.addUser({userid: 'McTesty', email: 'mctesty@tester.com', pwhash: 'test', extras: {junk: 'bar'}}, function(result){
+        dbmongo.addUser(user2, function(result){
             expect(result.success).to.be.true;
             done();
         });
     });
+
+    it('should find a user by username', function(done) {
+        dbmongo.getUser({user: user1.userid}, function(result) {
+            expect(result.success).to.be.true;
+            checkResult(result.user, user1);
+            done();
+        });
+    });
+
+    it('should find a user by email', function(done) {
+        dbmongo.getUser({user: user2.email}, function(result) {
+            expect(result.success).to.be.true;
+            checkResult(result.user, user2);
+            done();
+        });
+    });
+
+    it('should fail to find a nonexistent user by email', function(done) {
+        dbmongo.getUser({user: 't@mctester.com'}, function(result) {
+            expect(result.success).to.be.false;
+            done();
+        });
+    });
+
+    it('should fail to find a nonexistent user by name', function(done) {
+        dbmongo.getUser({user: 'foo'}, function(result) {
+            expect(result.success).to.be.false;
+            done();
+        });
+    });
+
 
     it('should fail to delete multiple users with wildcards', function(done) {
         // TODO: understand mongo wildcard behavior
@@ -71,7 +110,7 @@ describe('db_mongo', function() {
     });
 
     it('should fail to delete a user with an invalid password', function(done) {
-        dbmongo.deleteUser({userid: 'Testy', pwhash: 'zzz'}, function(result) {
+        dbmongo.deleteUser({userid: user1.userid, pwhash: 'zzz'}, function(result) {
             expect(result.success).to.be.false;
             expect(result.message).to.equal('User/password combination not found.');
             done();
@@ -79,7 +118,7 @@ describe('db_mongo', function() {
     });
 
     it('should delete a user with a valid password', function(done) {
-        dbmongo.deleteUser({userid: 'Testy', pwhash: 'test'}, function(result) {
+        dbmongo.deleteUser({userid: user1.userid, pwhash: user1.pwhash}, function(result) {
             expect(result.success).to.be.true;
             expect(result.message).to.equal('User permanently deleted.');
             done();
@@ -87,7 +126,7 @@ describe('db_mongo', function() {
     });
 
     it('should fail to delete a user with an invalid administrative key', function(done) {
-        dbmongo.deleteUser({userid: 'Testy', adminkey: 'zzz'}, function(result) {
+        dbmongo.deleteUser({userid: user2.userid, adminkey: 'zzz'}, function(result) {
             expect(result.success).to.be.false;
             expect(result.message).to.equal('Password not properly specified.');
             done();
@@ -95,7 +134,7 @@ describe('db_mongo', function() {
     });
 
     it('should delete a user with a valid administrative key', function(done) {
-        dbmongo.deleteUser({userid: 'McTesty', adminkey: 'specialkey'}, function(result) {
+        dbmongo.deleteUser({userid: user2.userid, adminkey: 'specialkey'}, function(result) {
             expect(result.success).to.be.true;
             expect(result.message).to.equal('User permanently deleted.');
             done();
