90a94ec14ed341317c3e29a37b92bcf1b5fa6eda
kentquirk <kentquirk@gmail.com>
2014-01-03 10:33:38 -0800

    I believe db_mongo is now in the kind of shape I want it in; now I can use it in the user API

--- test/test_db_mongo.js
+++ test/test_db_mongo.js
@@ -28,29 +28,38 @@ describe('db_mongo', function() {
             mongo_connection_string: 'mongodb://localhost/test',
             // the special config value we pass for testing will wipe the database
             _wipeTheEntireDatabase: true,
-            adminkey: 'specialkey'
+            adminkey: 'specialkey',
+            salt_deploy: 'randomsaltvalue'
         }, done);
     });
 
-    var user1 = {userid: 'Testy', email: 'mctesty@mctester.com', pwhash: 'test2', extras: {}};
-    var user2 = {userid: 'McTesty', email: 'mctesty@tester.com', pwhash: 'test', extras: {junk: 'bar'}};
+    var user1 = {username: 'Testy', emails: ['mctesty@mctester.com'], password: 'test2'};
+    var user2 = {username: 'McTesty', emails: ['mctesty@tester.com'], password: 'test'};
 
     var checkResult = function(user, ref) {
         expect(user).to.have.property('userid');
-        expect(user.userid).to.equal(ref.userid);
+        if (ref.userid) expect(user.userid).to.equal(ref.userid);
+        expect(user).to.not.have.property('password');
         expect(user).to.not.have.property('pwhash');
         expect(user).to.have.property('emails');
         expect(user.emails.length).to.equal(1);
-        expect(user.emails[0]).to.equal(ref.email);
-        expect(user).to.have.property('extras');
-        expect(user.extras).to.eql(ref.extras);
+        expect(user.emails[0]).to.equal(ref.emails[0]);
+        expect(user).to.have.property('userid');
+        expect(user.userid.length).to.equal(10);
+        expect(user).to.have.property('userhash');
     };
 
+    it('should have a good status return', function(done) {
+        expect(dbmongo.status()).to.be.true;
+        done();
+    });
 
     it('should create a user', function(done) {
         dbmongo.addUser(user1, function(result) {
             expect(result.success).to.be.true;
             checkResult(result.user, user1);
+            user1.userid = result.user.userid;
+            user1.userhash = result.user.userhash;
             done();
         });
     });
@@ -65,12 +74,15 @@ describe('db_mongo', function() {
     it('should create a second user', function(done) {
         dbmongo.addUser(user2, function(result){
             expect(result.success).to.be.true;
+            checkResult(result.user, user2);
+            user2.userid = result.user.userid;
+            user2.userhash = result.user.userhash;
             done();
         });
     });
 
     it('should find a user by username', function(done) {
-        dbmongo.getUser({user: user1.userid}, function(result) {
+        dbmongo.getUser({user: user1.username}, function(result) {
             expect(result.success).to.be.true;
             checkResult(result.user, user1);
             done();
@@ -78,9 +90,16 @@ describe('db_mongo', function() {
     });
 
     it('should find a user by email', function(done) {
-        dbmongo.getUser({user: user2.email}, function(result) {
+        dbmongo.getUser({user: user2.emails[0]}, function(result) {
             expect(result.success).to.be.true;
             checkResult(result.user, user2);
+            done();
+        });
+    });
+
+    it('should not die when given an empty query', function(done) {
+        dbmongo.getUser({user: undefined}, function(result) {
+            expect(result.success).to.be.false;
             done();
         });
     });
@@ -101,16 +120,24 @@ describe('db_mongo', function() {
 
 
     it('should fail to delete multiple users with wildcards', function(done) {
-        // TODO: understand mongo wildcard behavior
-        dbmongo.deleteUser({userid: '.*', pwhash: '.*'}, function(result) {
+        // wildcards don't work with mongo like this, but I want to be sure
+        dbmongo.deleteUser({userid: '.*', password: '.*'}, function(result) {
             expect(result.success).to.be.false;
             expect(result.message).to.equal('User/password combination not found.');
             done();
         });
     });
 
+    it('should fail to do anything if the userid is null', function(done) {
+        dbmongo.deleteUser({userid: null, password: 'zzz'}, function(result) {
+            expect(result.success).to.be.false;
+            expect(result.message).to.equal('User/password not properly specified.');
+            done();
+        });
+    });
+
     it('should fail to delete a user with an invalid password', function(done) {
-        dbmongo.deleteUser({userid: user1.userid, pwhash: 'zzz'}, function(result) {
+        dbmongo.deleteUser({userid: user1.userid, password: 'zzz'}, function(result) {
             expect(result.success).to.be.false;
             expect(result.message).to.equal('User/password combination not found.');
             done();
@@ -118,7 +145,7 @@ describe('db_mongo', function() {
     });
 
     it('should delete a user with a valid password', function(done) {
-        dbmongo.deleteUser({userid: user1.userid, pwhash: user1.pwhash}, function(result) {
+        dbmongo.deleteUser({userid: user1.userid, password: user1.password}, function(result) {
             expect(result.success).to.be.true;
             expect(result.message).to.equal('User permanently deleted.');
             done();
@@ -128,7 +155,7 @@ describe('db_mongo', function() {
     it('should fail to delete a user with an invalid administrative key', function(done) {
         dbmongo.deleteUser({userid: user2.userid, adminkey: 'zzz'}, function(result) {
             expect(result.success).to.be.false;
-            expect(result.message).to.equal('Password not properly specified.');
+            expect(result.message).to.equal('User/password not properly specified.');
             done();
         });
     });
